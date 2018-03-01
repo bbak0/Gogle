bcda8aff5982558b5992cd0a10533b69a69b679e
JoaoFerreira100 <32512196+JoaoFerreira100@users.noreply.github.com>
2018-03-01 18:57:33 +0100

    read

--- src/Problem.java
+++ src/Problem.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class Problem {
-    //dupex
+    Scanner scan = new Scanner(System.in);
+
+    int R; //rows
+    int C; //columns
+    int F; //number of vehicles
+    int N; //number of rides
+    int B; //Bonus
+    int T; //number of steps
+
+    void readInput() {
+        R = scan.nextInt();
+        C = scan.nextInt();
+        F = scan.nextInt();
+        N = scan.nextInt();
+        B = scan.nextInt();
+        T = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+
+        }
+    }
 }
